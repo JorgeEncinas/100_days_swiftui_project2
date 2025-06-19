376893e940adfb5e42120bd3949e18f9ca0131f5
Jorge Encinas <jorgecarlosencinas@gmail.com>
2025-06-19 16:16:14 -0700

    Colors and Frames

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,44 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack { // a 3*3 grid, though I doubt this is the intended way!
-            HStack {
-                Text("1")
-                Text("2")
-                Text("3")
+        ZStack() {
+            VStack(spacing: 0) {
+                Color.red
+                Color.blue
             }
-            HStack {
-                Text("4")
-                Text("5")
-                Text("6")
-            }
-            HStack {
-                Text("7")
-                Text("8")
-                Text("9")
-            }
-        }
-        
-        //ZStack(alignment: .top) { //No concept of spacing, but it `does` have alignment. Can align differently-sized things
-        //    Text("Hello, World!")
-        //    Text("This is inside a stack!")
-        //} //ZStack will draw things in this order: top to bottom, back to front. So if you place an image, then a text, that's the order, and so the text will end up on top.
-        
-        //VStack {
-        //    Text("First")
-        //    Text("Second")
-        //    Spacer() //Push contents of the stack to one side, taking all remaining space automatically.
-        //    Text("Third")
-        //    Spacer() // More than one spacer, then they divide the available space between them
-        //}
-        //VStack(alignment: .leading, spacing: 20) {
-        //    Text("Hello, world!")
-        //    Text("This is inside a stack")
-        //    HStack(spacing: 20) {
-        //        Text("Hello again, world!")
-        //        Text("This is inside an HStack")
-        //    }
-        //}
+            Text("Your content")
+                .foregroundStyle(.secondary)
+                .padding(50)
+                .background(.ultraThinMaterial)
+            
+            //Color(red: 1, green: 0.8, blue: 0) //Here it is not obvious by context if we were to write just .green, so now it is necessary for you to write Color.green
+                //.frame(minWidth: 200, maxWidth: .infinity, maxHeight: .infinity)
+            //Note you can't have min/max AND width: 200, height: 200 (unlike CSS)
+            //Text("Your content")
+            //    .background(.blue)
+        }//.background(.red) //This one changes from coloring just the text, to coloring everything! (and back to seemingly nothing.
+            .ignoresSafeArea()
     }
 }
 
@@ -60,6 +39,31 @@ struct ContentView: View {
 //      1. We can specify how much space to place btw views.
 //      2. We can specify an `alignment`: left, center, right
 //      3. If we don't, SwiftUI can freely arrange the views in a different way.
+
+// COLORS - DEFAULT VS SEMANTIC
+//  Some default colors are Color.blue, Color.green, Color.indigo
+//  SEMANTIC COLORS: They don't say what the HUE is, instead describing their PURPOSE
+//      Color.primary - SwiftUI's default Text Color (black/white depending on light/dark mode)
+//      Color.secondary - slight transparency so the color behind shines through (also has mode variations)
+
+//  SPECIFIC COLORS
+//  Color(red: _, green: _, blue: _)
+
+//  SAFE AREA
+//  You can put your ZStack past it with `.ignoresSafeArea()`
+//      Avoid putting important content outside the Safe Area though.
+//  Some views, such as List, allow content to scroll past the safe area, but add extra insets,
+//      so that the user can scroll things into view
+
+//  MATERIALS
+//  The background() modifier accepts `materials`
+//      which can apply a "frosted glass effect"
+//      over whatever is below them
+//  With this you can create Depth Effects.
+
+// VIBRANCY
+//  That effect where your colors in the background are bleeding into the text in the foreground (that is, it is allowed to 'come through')
+//  Just a hint of the color from the background.
 
 
 #Preview {
